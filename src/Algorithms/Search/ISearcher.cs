4764613084d4022b2b831c934c8c45e0b9728a95
szawrowski <alexander.szawrowski@gmail.com>
2024-07-31 08:47:55 +0300

    feat: Add ISearcher interface

---
+++ src/Algorithms/Search/ISearcher.cs
@@ -0,0 +1,21 @@
+﻿namespace Algorithms.Search;
+
+/// <summary>
+/// Defines a method for searching an item in a list.
+/// </summary>
+public interface ISearcher
+{
+    /// <summary>
+    /// Finds the index of the specified item in the provided list.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of elements in the list. This type must implement the IComparable{T} interface.
+    /// </typeparam>
+    /// <param name="data">The list of elements to search through.</param>
+    /// <param name="query">The item to search for in the list.</param>
+    /// <returns>
+    /// The index of the specified item if found; otherwise, -1.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">Thrown when data or query is null.</exception>
+    int FindIndex<T>(IList<T> data, T query) where T : IComparable<T>;
+}
