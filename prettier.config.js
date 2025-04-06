239d9e2a3ac02a63cdeed0681dacd9d9354d0a38
szawrowski <szawrowski.w3@gmail.com>
2025-04-06 19:28:38 +0300

    Add Prettier configuration

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+export default {
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  printWidth: 80,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
